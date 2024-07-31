5f0f754e86bc24d636d444f8ec0f71300d389f5b
hussainahmad1995 <76591561+hussainahmad1995@users.noreply.github.com>
2024-07-31 18:20:52 -0500

    simulation with no disturbance - for pretraining

--- workspace/PandasScripts/csvhelper_memento.py
+++ workspace/PandasScripts/csvhelper_memento.py
@@ -148,7 +148,7 @@ def generate_senders_csv(path, n_senders):
     # ]
     # files = ["memento_test10.csv", "memento_test20.csv", "memento_test25.csv"]
 
-    files = ["small_test_one_disturbance_with_message_ids1.csv"]
+    files = ["small_test_no_disturbance_with_message_ids1.csv"]
 
     for file in files:
         print("The output of a file " + file)
